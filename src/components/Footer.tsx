18086b9bf417bbdfa1f1644d63a2fcc9268752e2
Jay Ghevariya <jayghevariya1@gmail.com>
2026-02-04 16:47:35 +0530

    Update 2.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = memo(() => {
     { to: "/dashboard", label: "Dashboard" },
     { to: "/map", label: "Map Explorer" },
     { to: "/explore", label: "Explore" },
-    { to: "/community", label: "Community" },
+    { to: "/analytic", label: "AnalyticHub" },
     { to: "/learn", label: "Learn" },
     { to: "/about", label: "About" },
   ];
